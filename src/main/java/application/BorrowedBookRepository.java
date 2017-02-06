a92624acec61270f63a0927710a825937c093a54
Tony Jones <xm39@global.tesco.org>
2017-02-06 21:13:34 +0000

    correction for inDate update on the database.

--- src/main/java/application/BorrowedBookRepository.java
+++ src/main/java/application/BorrowedBookRepository.java
@@ -1,19 +1,7 @@
 package application;
 
 public interface BorrowedBookRepository {
-
-
-    /**************
-     *
-     * @param borrowedBook
-     * @return
-     *
-     * You require a function to insert an record of a book being issues to
-     * a library member.
-     *
-     */
     public BorrowedBook insertBorrowedBook(BorrowedBook borrowedBook);
-
     public void listBorrowedBooks();
     public BorrowedBook returnBorrowedBook(BorrowedBook borrowedBook);
     public int countBooks();
